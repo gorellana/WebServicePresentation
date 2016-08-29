1fd928470942d1c4f99454799921c233a6bffe41
gorellana <gil.orellana@gmail.com>
2016-08-29 04:04:27 -0400

    added more routes.  code clean up

--- MagicEightBallCore/Magic.cs
+++ MagicEightBallCore/Magic.cs
@@ -10,23 +10,30 @@ namespace MagicEightBallCore
     {
         public static string GetAnswer(string userQuestion)
         {
+            if (string.IsNullOrEmpty(userQuestion))
+            {
+                return "What is your question?";
+            }
+
             string question = userQuestion.ToLower();            
 
             if (question.Contains("name") == true)            
-                return "I dont know anything about names";
+                return "What's in a name anyway?";
 
             if (question.Contains("who") == true)
-                return "who... your mama";
+                return "who... your mama!";
 
             if (question.Contains("what") == true)            
                 return "Yes or No questions please.";            
 
             if (question.Contains("where") == true)            
-                return "in your under wear!";
-            
+                return "in your under wear!";            
 
             if (question.Contains("when") == true)            
-                return "never!";            
+                return "never!";
+
+            if (question.Contains("how") == true)
+                return "how do you not know how to answer a Yes or No quesiton?";
 
             if (question.Contains("gil") == true)
                 return "gil is magic";            
@@ -40,6 +47,13 @@ namespace MagicEightBallCore
             Random rnd = new Random();
             int questionNumber = rnd.Next(0, 20);
 
+            List<string> answerList = GetAnswerList();            
+            return answerList[questionNumber];
+        }
+
+
+        public static List<string> GetAnswerList()
+        {
             List<string> answerList = new List<string>();
             answerList.Add("It is certain");
             answerList.Add("It is decidedly so");
@@ -57,13 +71,13 @@ namespace MagicEightBallCore
             answerList.Add("Cannot predict now");
             answerList.Add("Concentrate and ask again");
             answerList.Add("Don't count on it");
-            answerList.Add("My reply is no");
+            answerList.Add("My reply is F no!");
             answerList.Add("Jess says no");
             answerList.Add("Outlook not so good");
             answerList.Add("Very doubtful");
             answerList.Add("You are sleeping, you don not want to believe");
 
-            return answerList[questionNumber];
+            return answerList;
         }
     }
 }
--- MagicEightBallRest/Controllers/QuestionController.cs
+++ MagicEightBallRest/Controllers/QuestionController.cs
@@ -1,4 +1,5 @@
-﻿using MagicEightBallWcf;
+﻿using MagicEightBallCore;
+using MagicEightBallWcf;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,36 +8,57 @@ using System.Net.Http;
 using System.Web.Http;
 
 namespace MagicEightBallRest.Controllers
-{   
-
+{
+    //http://localhost/magicEightBallRest/api/answers?question=how%20do%20you%20do%20it
     public class AnswerController : ApiController
     {
-       
-        [Route("api/answers")]      
+        // Gets All the answers
+        [Route("api/answer")]
         [HttpGet]
-        public string GetAnswer(string question)
+        public List<string> GetAnswer()
         {
-            if (question.Contains("name") == true)
-            {
-                return "I dont know anything about names";
-            }
-
-            MagicEightBallService wcf = new MagicEightBallService();
-            return wcf.SubmitQuestion(question);            
+           return Magic.GetAnswerList();
         }
 
+        //Gets the answer that correstponds to the number the user typed in
+        [Route("api/answer/{questionNumber}")]
+        [HttpGet]
+        public string GetAnswer(int questionNumber)
+        {
+           
+            List<string> answers = Magic.GetAnswerList();
+               
+            if (questionNumber >= 0 && questionNumber <= answers.Count)
+            {
+                 return string.Format("Question {0} is: {1}", questionNumber, answers[questionNumber - 1]);
+            }           
+                       
+             return string.Format("Queseton {0} does not exist",questionNumber);                       
+        }
 
-        [Route("api/answers")]
+        // answer based on query string ?question=is my name
+        [Route("api/answer")]      
+        [HttpGet]
+        public string AskQuestion(string question)
+        {
+            int number;
+            bool isNumber = int.TryParse(question, out number);
+            if (!int.TryParse(question, out number) )
+            {
+                return Magic.GetAnswer(question);
+            }
+
+            return string.Format("{0} What?... that is not a question.",question);
+                               
+        }
+
+        [Route("api/answer")]
         [HttpGet]
         public string GetAnswer(string question, string name)
         {
-           
-
             MagicEightBallService wcf = new MagicEightBallService();
             return wcf.SubmitQuestion(question);
         }
-
-
 
         [Route("api/orders/{id}")]
         [HttpGet]
@@ -44,6 +66,5 @@ namespace MagicEightBallRest.Controllers
         {
             return string.Format("Your Order {0} contains french fries",id);
         }
-
     }
 }
